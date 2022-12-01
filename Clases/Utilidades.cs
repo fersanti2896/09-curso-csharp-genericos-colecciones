6b621c91bfa5009e2fe4efcab58e26cbf48a14a5
Fernando Nicolás <fersanti2896@gmail.com>
2022-12-01 11:02:27 -0600

    Genéricos y restricciones en Program.cs

---
+++ Clases/Utilidades.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genericos.Clases {
+    internal class Utilidades<T> {
+        internal int longitudArreglo(T[] arreglo) {
+            return arreglo.Length;
+        }
+
+        internal T? primerElementoOPorDefecto(T[] arreglo) {
+            if (arreglo.Length == 0) {
+                return default;
+            }
+
+            return arreglo[0];
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,31 @@
 ﻿
 
+using Genericos.Clases;
+
 Console.WriteLine("GENÉRICOS Y COLECCIONES\n");
+
+int[] numeros = { 1, 2, 3 };
+string[] nombres = { "Fernando", "María", "Marisol", "Luis" };
+DateTime[] fechas = { DateTime.Today, DateTime.Today.AddDays(1) };
+
+var utilidades = new Utilidades<string>();
+
+Console.WriteLine($"Longitud de arreglo de numeros: { longitudArreglo(numeros) }");
+Console.WriteLine($"Longitud de arreglo de nombres: { longitudArreglo(nombres) }");
+Console.WriteLine($"Longitud de arreglo de fechas: { longitudArreglo(fechas) } \n");
+
+Console.WriteLine($"Primer elemento de numeros: { primerElementoOPorDefecto(numeros) }");
+Console.WriteLine($"Primer elemento de nombres: { primerElementoOPorDefecto(nombres) }");
+Console.WriteLine($"Primer elemento de fechas: { primerElementoOPorDefecto(fechas) } \n");
+
+int longitudArreglo<T>(T[] arreglo) where T: notnull {
+    return arreglo.Length;
+}
+
+T? primerElementoOPorDefecto<T>(T[] arreglo) {
+    if (arreglo.Length == 0) {
+        return default;
+    }
+
+    return arreglo[0];
+}
